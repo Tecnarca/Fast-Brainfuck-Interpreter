2d8c3fcbacd2ec3ef98b1ce0e1d01e4bf0c9bad3
Tecnarca <mail.tecnarca@gmail.com>
2018-10-13 12:13:57 +0200

    Prepared files for pubblication

---
+++ BFinterpreter.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <time.h>
+
+#define PROGRAM_SIZE    404096 //max program size
+#define DATA_SIZE       605535
+#define OVERFLOW -1
+#define UNDERFLOW -2
+
+int interpreter(char* prog){
+	
+	char* ist; //current istruction
+	char* JMP[PROGRAM_SIZE]; //istructions that are beginning of cycles ("[")
+	int cr_jmp=0; //index of the next element of JMP
+	int count;
+	unsigned char data[DATA_SIZE] = {0}; //data space
+	unsigned int ptr=0; //pointer that moves along the data space
+	
+	for(ist=prog;*ist;ist++){
+		switch(*ist){
+			case '>':
+				if(ptr==DATA_SIZE) return OVERFLOW; 
+				else ptr++;
+				break;
+
+			case '<':
+				if(ptr==0) return UNDERFLOW;
+				else ptr--;
+				break;
+
+			case '+':
+				data[ptr]++;
+				break;
+
+			case '-':
+				data[ptr]--;
+				break;
+
+			case '.':
+				putchar(data[ptr]);
+				break;
+
+			case ',':
+				data[ptr] = (unsigned char)getchar();
+				break;
+
+			case '[':
+				if(data[ptr]){
+					JMP[cr_jmp]=ist;
+					cr_jmp++;
+				} else {
+					cont = 1;
+					for(ist++;count&&*ist;ist++){
+						if(*ist == '[') count++;
+						if(*ist == ']') count--;
+					}
+					ist--;
+				}
+				
+				break;
+
+			case ']':
+				if(cr_jmp>0)
+					if(data[ptr]) ist=JMP[cr_jmp-1];
+					else cr_jmp--;
+				break;
+		}
+	}
+	return 0;
+}
+
+
+int main(int argc, char **argv){
+	
+	if (argc != 2 ) {
+        fprintf(stderr, "Usage: %s [file_to_interpret]\n", argv[0]);
+        return 0;
+    }
+
+    printf("\n");
+    
+    FILE *fp;
+    fp = fopen(argv[1],"r");
+    int i=0;
+    char prog[PROGRAM_SIZE];
+    for(; (prog[i]=fgetc(fp))!=EOF;i++);
+    prog[i]='\0';
+
+	clock_t startTime = clock();
+
+    int c = interpreter(prog);
+
+    if(c != 0) printf("\nExecuted in %f seconds.\n\n", ((float) clock() - startTime ) / (float)CLOCKS_PER_SEC);
+	else printf("Program execution failed, returned %d code", c);
+
+	return 0;
+}
+    
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Fast-Brainfuck-Interpreter
-An interpreter for the brainfuck language written to be as fast as possible
+An interpreter for the brainfuck language written to be as fast as possible.
+
+The program is written in C and it uses the time.h library to measure how many seconds the interpreter function takes to finish.
+
+## Compiling and usage
+To compile and execute, use the commands:
+```
+gcc -o BFinterpreter.exe BFinterpreter.c 
+./BFinterpreter.exe [FILE_TO_EXECUTE]
+```
+`FILE_TO_EXECUTE` must be a brainfuck program. 
+Every non standard brainfuck letter will be ignored by the program (so you can just type characters in there as comments and the interpreter will not fail).
+
+## Provided Files
+`Hanoi_Tower.bf` (computes graphically the hanoi tower) and `mandelbrot_fractal.bf` (computes graphically the mandelbrot fractal) are brainfuck programs (just text) that are ready to execute and can be used to compare speeds with other brainfuck interpreters
